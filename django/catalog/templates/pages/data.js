5f755e0991389ec920ef5646e1ae5a0ceff888d5
Tunahan Berber <tunahanberber123@gmail.com>
2021-10-19 00:15:32 +0300

    first commit

---
+++ django/catalog/templates/pages/data.js
@@ -0,0 +1,18 @@
+var dummyData = [
+    {
+        name: "Deniz",
+        desc: "asldkjasd laskdjl ajdaslkj laskdj lorem ipsum"
+    },
+    {
+        name: "alskd",
+        desc: "asldkjasd laskdjl ajdaslkj laskdj lorem ipsum"
+    },
+    {
+        name: "asdasd",
+        desc: "asldkjasd laskdjl ajdaslkj laskdj lorem ipsum"
+    },
+    {
+        name: "Denasdiz",
+        desc: "lorem ipsum dolor sit amet"
+    }
+]
